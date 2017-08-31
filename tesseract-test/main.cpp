f16e1ad99421cb5eeff0b041b1edfbc460d2176d
李振洋 <lizhenyang@raintai.com>
2017-08-31 17:57:17 +0800

    Initial Commit

---
+++ tesseract-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tesseract-test
+//
+//  Created by cslzy on 8/31/17.
+//  Copyright © 2017 CY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
